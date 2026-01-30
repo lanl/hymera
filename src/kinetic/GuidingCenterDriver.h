39469527744fce30eea0b21efbbd80fe864507dd
Oleksii Beznosov <obeznosov@lanl.gov>
2026-01-30 09:39:16 -0800

    Added GCE driver

---
+++ src/kinetic/GuidingCenterDriver.h
@@ -0,0 +1,40 @@
+//========================================================================================
+// (C) (or copyright) 2025. Triad National Security, LLC. All rights reserved.
+//
+// This program was produced under U.S. Government contract 89233218CNA000001 for Los
+// Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC // for the U.S. Department of Energy/National Nuclear Security Administration. All rights
+// in the program are reserved by Triad National Security, LLC, and the U.S. Department
+// of Energy/National Nuclear Security Administration. The Government is granted for
+// itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+// license in this material to reproduce, prepare derivative works, distribute copies to
+// the public, perform publicly and display publicly, and to permit others to do so.
+//========================================================================================
+#pragma once
+#include <parthenon/driver.hpp>
+
+namespace Kinetic {
+
+using namespace parthenon;
+using namespace parthenon::driver::prelude;
+
+class GuidingCenterDriver: public EvolutionDriver {
+public:
+  GuidingCenterDriver(ParameterInput *pin, ApplicationInput *app_in, Mesh *pmesh)
+      : EvolutionDriver(pin, app_in, pmesh) {
+  }
+
+  TaskListStatus Step() {
+    PARTHENON_INSTRUMENT
+    using DriverUtils::ConstructAndExecuteTaskLists;
+    TaskListStatus status = ConstructAndExecuteTaskLists<>(this, tm);
+    return status;
+  };
+
+  void PreExecute();
+  void PostExecute(parthenon::DriverStatus st);
+
+  TaskCollection MakeTaskCollection(BlockList_t &blocks, SimTime tm);
+
+};
+}
+
